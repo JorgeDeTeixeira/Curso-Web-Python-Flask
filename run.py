2c913f0053bbf463e8bf68813ec824b6488faa35
Jorge Teixeira <jorgedeteixeirademelo@gmail.com>
2024-01-22 11:16:14 -0300

    Primeiro projeto básico

--- run.py
+++ run.py
@@ -1,12 +1,4 @@
-from flask import Flask
-
-app = Flask(__name__)
-
-
-@app.route('/')
-def homepage():
-    return "Minha primeira página flask"
-
+from app import app
 
 if __name__ == "__main__":
     app.run(debug=True)
