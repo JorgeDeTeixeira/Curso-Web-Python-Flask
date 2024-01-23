47408dd058989f83bef9d8de5e7d3a3cc4eb7ed6
Jorge Teixeira <jorgedeteixeirademelo@gmail.com>
2024-01-23 08:50:42 -0300

    Enviando dados do servidor para o navegador

--- app/routes.py
+++ app/routes.py
@@ -1,6 +1,11 @@
 from flask import render_template, url_for
 from app import app
 
+
 @app.route("/")
 def homepage():
-    return render_template('index.html')
+    context = {
+        'usuario': 'Jorge',
+        'idade': 20
+    }
+    return render_template('index.html', context=context)
