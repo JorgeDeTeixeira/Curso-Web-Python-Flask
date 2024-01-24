6e4b07ba9a164f40120f428c5fc02645a04763b8
Jorge Teixeira <jorgedeteixeirademelo@gmail.com>
2024-01-24 09:33:34 -0300

    Criando rota para login

--- app/forms.py
+++ app/forms.py
@@ -56,3 +56,18 @@ class UserForm(FlaskForm):
     def getUser(self):
         self.save()
         return User.query.filter_by(username=self.username.data).first()
+
+class LoginForm(FlaskForm):
+    username = StringField('Username', validators=[DataRequired()])
+    senha = PasswordField('Senha', validators=[DataRequired()])
+    submit = SubmitField('Login')
+    
+    def login(self):
+        user = User.query.filter_by(username=self.username.data).first()
+        if user:
+            if bcrypt.check_password_hash(user.password, self.senha.data):
+                return user
+            else:
+                raise Exception('Senha incorreta!')
+        else:
+            raise Exception("Usuário não encontrado!")
--- app/routes.py
+++ app/routes.py
@@ -1,6 +1,6 @@
 from flask import render_template, url_for, jsonify, redirect
 from app import app
-from app.forms import FilmeForm, UserForm
+from app.forms import FilmeForm, UserForm, LoginForm
 from app.models import Filme
 from flask_login import login_required, login_user, logout_user
 
@@ -58,3 +58,13 @@ def newUser():
         login_user(user, remember=True)
         return redirect(url_for('homepage'))
     return render_template('novoUsuario.html', form=form)
+
+
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    form = LoginForm()
+    if form.validate_on_submit():
+        user = form.login()
+        login_user(user, remember=True)
+        return redirect(url_for('homepage'))
+    return render_template('login.html', form=form)
