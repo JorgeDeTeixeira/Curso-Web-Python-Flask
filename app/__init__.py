9a72ffd8518fc7c15bb8b40bb5bd1f81736e0a96
Jorge Teixeira <jorgedeteixeirademelo@gmail.com>
2024-01-24 07:45:11 -0300

    Configurando a aplicação para usar login e criando o model do user

--- app/__init__.py
+++ app/__init__.py
@@ -1,6 +1,9 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
+from flask_login import LoginManager
+from flask_bcrypt import Bcrypt
+
 from local_settings import SECRET_KEY
 
 app = Flask(__name__)
@@ -12,6 +15,9 @@ app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
 db = SQLAlchemy(app)
 migrate = Migrate(app, db)
+login_manager = LoginManager(app)
+login_manager.login_view = 'login'
+bcrypt = Bcrypt(app)
 
 from app import routes
 from app import models
--- app/models.py
+++ app/models.py
@@ -1,5 +1,11 @@
-from app import db
+from app import db, login_manager
 from datetime import datetime
+from flask_login import UserMixin
+
+
+@login_manager.user_loader
+def load_user(user_id):
+    return User.query.get(int(user_id))
 
 
 class Filme(db.Model):
@@ -9,3 +15,12 @@ class Filme(db.Model):
     titulo = db.Column(db.String(50), nullable=True)
     ano = db.Column(db.Integer, nullable=True)
     resumo = db.Column(db.String(1000), nullable=True)
+
+
+class User(db.Model, UserMixin):
+    id = db.Column(db.Integer, primary_key=True)
+    email = db.Column(db.String(200), unique=True)
+    username = db.Column(db.String(100))
+    nome = db.Column(db.String(100))
+    sobrenome = db.Column(db.String(100))
+    password = db.Column(db.String(100))
